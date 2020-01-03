09b1762f2926a56a688f24684cae137e918b60dc
Nick T <17ndt@queensu.ca>
2020-01-03 07:33:59 -0500

    Started Adding in Client Credentials

--- spotify.py
+++ spotify.py
@@ -1,5 +1,18 @@
 # Spotify Toolkit Version 1
 # Nick Totman
 
-# I think this will make a change
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
+
+with open('Account.txt', 'r') as f:
+    x = f.read().splitlines()
+
+client_ID = x[5]
+client_secret = x[7]
+
+print('Client ID: ' + client_ID)
+print('Client Secret: ' + client_secret)
+
+client_credentials_manager = SpotifyClientCredentials(client_id=client_ID, client_secret=client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
