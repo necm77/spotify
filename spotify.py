a196ec65a61bbce4a56c484a8e9307bf8b97f982
Nick T <17ndt@queensu.ca>
2020-01-04 07:34:50 -0500

    Access to Spotify API Complete!

--- spotify.py
+++ spotify.py
@@ -1,6 +1,5 @@
 # Spotify Toolkit Version 1
 # Nick Totman
-# Run this from terminal??
 
 import sys
 import spotipy
@@ -10,27 +9,22 @@ from spotipy.oauth2 import SpotifyClientCredentials
 # Import saved information to access Spotify API
 with open('Account.txt', 'r') as f:
     x = f.read().splitlines()
+username = x[1]
 client_ID = x[5]
 client_secret = x[7]
-redirect_uri = "http://localhost:8888/callback/"
+redirect_uri = x[9]
+print('Username: ' + username)
 print('Client ID: ' + client_ID)
 print('Client Secret: ' + client_secret)
+print('Redirect URI: ' + redirect_uri)
 
-# Create Token (?) to be able to access Spotify API
-client_credentials_manager = SpotifyClientCredentials(client_id=client_ID, client_secret=client_secret)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-
-# Sign into Spotify
+# Create Token (?) to be able to access Spotify API & Sign into Spotify
+# Authorization Code Flow
 print('Reminder, your password is: ' + x[3])
-if len(sys.argv) > 1:
-    username = sys.argv[1]
-else:
-    print("Usage: %s username" % (sys.argv[0],))
-    sys.exit()
+scope = 'playlist-read-private'
 
-scope = ''
 token = util.prompt_for_user_token(username, scope, client_ID, client_secret, redirect_uri)
-
+sp = spotipy.Spotify(auth=token)
 if token:
     print('Looks good Nick!')
 else:
